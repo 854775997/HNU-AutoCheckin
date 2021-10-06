05cb468a2a34e075dca48f3cd46f092883e09194
Yuanchang-Huang <54267930+Yuanchang-Huang@users.noreply.github.com>
2021-10-06 11:34:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -107,9 +107,9 @@ class Checker(httpx.Client):
                     		"IsVia": "0",
                     		"DateTrip": ""
                 	},
-			"IsInCampus": "0",
-                	"IsViaHuBei": "0",
-                	"IsViaWuHan": "0",
+			"IsInCampus": "1",
+                	# "IsViaHuBei": "0",
+                	# "IsViaWuHan": "0",
 			"InsulatedAddress": "",
                 	"TouchInfo": "",
 			"IsNormalTemperature": "1",
